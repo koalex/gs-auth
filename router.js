5cfaef90429578d9762e314a126d47b9d1210540
Konstantin Aleksandrov <alexandrovkm@gmail.com>
2019-06-18 15:35:41 +0700

    Add passport middleware for routes

--- router.js
+++ router.js
@@ -9,7 +9,7 @@ const forgotPassword = require('./controllers/forgotPassword/index');
 const resetPassword  = require('./controllers/resetPassword');
 const signout        = require('./controllers/signout');
 const refreshTokens  = require('./controllers/refreshTokens');
-const authMW         = require('./middlewares/jwtAuth');
+const passport       = require('./middlewares/passport');
 const bodyParser     = require('gen-server/middlewares/bodyParser');
 
 const bodyParserOpts = { formLimit: '28kb', jsonLimit: '28kb', textLimit: '28kb' };
@@ -19,13 +19,15 @@ router.get('/signup', ctx => ctx.body = 'SIGNUP HTML PAGE');
 router.get('/forgot-password', ctx => ctx.body = 'FORGOT PASSWORD HTML PAGE');
 router.get('/users/:userId/password-reset/:token', resetPassword.get);
 
-ApiV1.post('/signout', bodyParser(bodyParserOpts), authMW, signout.single);
-ApiV1.post('/signout-all', bodyParser(bodyParserOpts), authMW, signout.all);
-ApiV1.post('/signin', bodyParser(bodyParserOpts), signin);
+// router.get('/signout', passport.authenticate('jwt', {session: false}), signout.single);
+// router.get('/signout-all', passport.authenticate('jwt', {session: false}), signout.all);
+ApiV1.post('/signout', bodyParser(bodyParserOpts), passport.authenticate('jwt', {session: false}), signout.single);
+ApiV1.post('/signout-all', bodyParser(bodyParserOpts), passport.authenticate('jwt', {session: false}), signout.all);
+ApiV1.post('/signin', bodyParser(bodyParserOpts), passport.authenticate('local', {session: false}), signin);
 ApiV1.post('/signup', bodyParser(bodyParserOpts), signup);
 ApiV1.post('/forgot-password', bodyParser(bodyParserOpts), forgotPassword);
 ApiV1.post('/users/:userId/password-reset/:token', bodyParser(bodyParserOpts), resetPassword.post);
 ApiV1.post('/auth/refresh-tokens', bodyParser(bodyParserOpts), refreshTokens);
-ApiV1.get('/me', authMW, async ctx => { ctx.body = ctx.state.user; });
+ApiV1.get('/me', passport.authenticate('jwt', {session: false}), async ctx => { ctx.body = ctx.state.user; });
 
 module.exports = [router, ApiV1];
